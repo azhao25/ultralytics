2076cd5163f7c03fe43c18aa783903bcb60e0d04
Andrew Zhao <aazhao@mit.edu>
2025-03-19 12:51:04 -0400

    attempt to do James' HSV changes

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1366,18 +1366,25 @@ class RandomHSV:
         """
         if self.hgain or self.sgain or self.vgain:
             img = labels["img"]
-            dtype = img.dtype  # uint8
+            # dtype = img.dtype  # uint8
+            dtype = np.uint8
 
             r = np.random.uniform(-1, 1, 3) * [self.hgain, self.sgain, self.vgain]  # random gains
-            x = np.arange(0, 256, dtype=r.dtype)
+            x = np.arange(0, 2**8, dtype=r.dtype)
             # lut_hue = ((x * (r[0] + 1)) % 180).astype(dtype)   # original hue implementation from ultralytics<=8.3.78
-            lut_hue = ((x + r[0] * 180) % 180).astype(dtype)
-            lut_sat = np.clip(x * (r[1] + 1), 0, 255).astype(dtype)
-            lut_val = np.clip(x * (r[2] + 1), 0, 255).astype(dtype)
+            # lut_hue = ((x + r[0] * 180) % 180).astype(dtype)
+            lut_hue = ((x * r[0]) % 180).astype(dtype)
+            
+            # lut_sat = np.clip(x * (r[1] + 1), 0, 2**8 - 1).astype(dtype)
+            # lut_val = np.clip(x * (r[2] + 1), 0, 2**8 - 1).astype(dtype)
+            lut_sat = np.clip(x * r[1], 0, 2**8-1).astype(dtype)
+            lut_val = np.clip(x * r[2], 0, 2**8-1).astype(dtype)
             lut_sat[0] = 0  # prevent pure white changing color, introduced in 8.3.79
 
             hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+
             im_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+            
             cv2.cvtColor(im_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
         return labels
 
@@ -2435,7 +2442,7 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
             pre_transform,
             MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
             Albumentations(p=1.0),
-            RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
+            # RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
             RandomFlip(direction="vertical", p=hyp.flipud),
             RandomFlip(direction="horizontal", p=hyp.fliplr, flip_idx=flip_idx),
         ]
